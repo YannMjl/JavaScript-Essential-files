1d282aa7098cc93119f906abfc516fe55a4b862b
Yann Mulonda <yannmjl@gmail.com>
2018-05-28 15:16:31 -0500

    added conditional logic and statements in main.js

--- main.js
+++ main.js
@@ -7,3 +7,21 @@ console.log("Hello World");
 var date = new Date();
 document.body.innerHTML = "<h1> Today's day is " + (date.getMonth() + 1) + "/" +
   date.getDate() + "/" + date.getFullYear() + "</h1>";
+
+var a = 3;
+var b = 5;
+var sum = a + b;
+var theNumbermatched;
+
+var humanSum = "The value of a is: " + a + ". The value of b is: " + b +
+  ". The Sum of a and b is equal to: " + sum + ".";
+
+console.log(humanSum);
+
+if (a == b) {
+  theNumbermatched = true;
+} else {
+  theNumbermatched = false;
+}
+
+console.log('Do the numbers matched? ' + theNumbermatched);
