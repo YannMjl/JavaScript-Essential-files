edd2a0d4cde90ec84889b9374a0e4379ec4ef9d1
Yann Mulonda <yannmjl@gmail.com>
2018-08-02 21:54:39 -0500

    added event listener

--- js/eventsHandler.js
+++ js/eventsHandler.js
@@ -1,12 +1,20 @@
+// Event handlers - listeners
 const CTA = document.querySelector(".cta a");
 const ALERT = document.querySelector("#booking-alert");
 
 CTA.classList.remove("hide");
 ALERT.classList.remove("hide");
 
-function reveal() {
+function reveal(e) {
+  // this prevent the default behavoir of the page which is cliking in this case
+  e.preventDefault();
+
   CTA.classList.toggle("hide");
   ALERT.classList.toggle("hide");
 }
 
-CTA.onclick = reveal;
+// event handler
+//CTA.onclick = reveal;
+
+// Event listeners
+CTA.addEventListner("click", reveal, false);
