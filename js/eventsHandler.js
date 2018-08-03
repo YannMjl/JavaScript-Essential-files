1c14389cbedab00cd579cb6e188a9de41b652578
Yann Mulonda <yannmjl@gmail.com>
2018-08-02 22:05:27 -0500

    pass arguments through event listeners

--- js/eventsHandler.js
+++ js/eventsHandler.js
@@ -1,15 +1,16 @@
 // Event handlers - listeners
 const CTA = document.querySelector(".cta a");
-const ALERT = document.querySelector("#booking-alert");
+const ALERT = document.querySelector("#Change-color");
 
 CTA.classList.remove("hide");
 ALERT.classList.remove("hide");
 
-function reveal(e) {
+function reveal(e, current) {
   // this prevent the default behavoir of the page which is cliking in this case
   e.preventDefault();
 
-  CTA.classList.toggle("hide");
+  current.innerHTML = "Change color" ? CTA.innerHTML = "Oooops!" : CTA.innerHTML = "Change Color";
+
   ALERT.classList.toggle("hide");
 }
 
@@ -17,4 +18,5 @@ function reveal(e) {
 //CTA.onclick = reveal;
 
 // Event listeners
-CTA.addEventListner("click", reveal, false);
+CTA.addEventListner("click", function(e){ reveal(e, this); }, false);
+CTA.addEventListner("click", function(){ console.log("The button was clicked"); }, false);
